1b85496c785f77ce2bfb90231bf645874ef3a95d
arifinlenna <arifin@lenna.ai>
2023-11-28 15:58:36 +0700

    sss

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -183,13 +183,13 @@ class UserController extends Controller
     *       summary="update User",
     *       description="update User",
     *     @OA\Parameter(
-    *          name="id",
-    *          description="identity",
-    *          required=true,
-    *          in="query",
-    *          @OA\Schema(
-    *              type="integer"
-    *          )
+    *         description="Parameter with mutliple examples",
+    *         in="path",
+    *         name="id",
+    *         required=true,
+    *         @OA\Schema(type="string"),
+    *         @OA\Examples(example="int", value="1", summary="An int value."),
+    *         @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
     *     ),
     *    @OA\RequestBody(
     *         @OA\MediaType(
